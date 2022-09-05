663e6170cc17d55c6f075d3ef9c3bc40a519cdc2
Bz-Carter <lebz2016@gmail.com>
2022-09-05 00:47:48 +0000

    first commit

---
+++ src/app/interfaces/mvolaiserv.ts
@@ -0,0 +1,8 @@
+export interface Mvolaiserv {
+  id: number;
+  name: string;
+  image: string;
+  body: string;
+  description: string;
+  created: Date;
+}
---
+++ src/app/interfaces/response2.ts
@@ -0,0 +1,6 @@
+export interface Response2 {
+  data: any;
+  meta?: {
+    count: number;
+  }
+}
